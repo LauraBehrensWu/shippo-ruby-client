425923bffabae6a93c284ccafdbb87e6e88c71d2
Subhi Beidas <sbeidas@iit.edu>
2015-07-06 10:16:06 -0700

    Added support for carrier accounts

---
+++ lib/shippo/update.rb
@@ -0,0 +1,16 @@
+module Shippo
+  module Operations
+    module Update
+      module ClassMethods
+        def update(object_id, params={})
+          #response = Shippo.request(:put, "#{url}/#{CGI.escape(object_id)}/", params)
+          puts params
+          #self.construct_from(response)
+        end
+      end
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+    end
+  end
+end
